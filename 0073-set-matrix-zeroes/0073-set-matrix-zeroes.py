70895b943c083f02ba042bbcf163ab5e41d2c5f2
Nihal Kumar <smartykavit@gmail.com>
2026-09-12 06:04:11 +0530

    Time: 11 ms (23.47%), Space: 20.9 MB (35.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,22 +1,27 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         r = len(matrix)
         c = len(matrix[0])
-
-        row_track = [0 for _ in range(r)]
-        col_track = [0 for _ in range(c)]
-
-        for i in range(r):
-            for j in range(c):
+        
+        first_row_has_zero = any(matrix[0][j] == 0 for j in range(c))
+        first_col_has_zero = any(matrix[i][0] == 0 for i in range(r))
+        
+        for i in range(1, r):
+            for j in range(1, c):
                 if matrix[i][j] == 0:
-                    row_track[i] = float("-inf")
-                    col_track[j] = float("-inf")
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
 
-        for i in range(r):
+        for i in range(1, r):
+            for j in range(1, c):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        
+        if first_row_has_zero:
             for j in range(c):
-                if row_track[i] == float("-inf") or col_track[j] == float("-inf"):
-                    matrix[i][j]=0
+                matrix[0][j] = 0
+        if first_col_has_zero:
+            for i in range(r):
+                matrix[i][0] = 0
+        
         return matrix
